4c008ba78405a853cdf47536862b914404ddce67
kiluchandran <kiluchandran@gmail.com>
2024-02-27 17:01:20 +0530

    Used string.ascii_letters to get letters in lower and upper case

--- generator_kilu.py
+++ generator_kilu.py
@@ -3,7 +3,7 @@ import string
 
 
 def generate_random_string(length):
-    letters = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    letters = string.ascii_letters + string.digits
     random_string = ''.join(random.choices(letters, k=length))
     return random_string
 
