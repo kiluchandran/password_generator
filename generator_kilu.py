0d487ddc0615736e60fe87ef7e9f1118b6737164
kiluchandran <kiluchandran@gmail.com>
2024-02-27 16:45:44 +0530

    Called and defined a function generate_random_string

--- generator_kilu.py
+++ generator_kilu.py
@@ -2,10 +2,14 @@ import random
 import string
 
 
-def main():
-    N = 7
+def generate_random_string(length):
     letters = string.ascii_uppercase + string.ascii_lowercase + string.digits
-    password = ''.join(random.choices(letters, k=N))
+    random_string = ''.join(random.choices(letters, k=length))
+    return random_string
+
+
+def main():
+    password = generate_random_string(7)
     print(password)
 
 
