914225da359719a28182b2eaa8ced4fe5e72b337
kiluchandran <kiluchandran@gmail.com>
2024-02-27 17:11:11 +0530

    Used random.choice() function

--- generator_kilu.py
+++ generator_kilu.py
@@ -4,7 +4,7 @@ import string
 
 def generate_random_string(length):
     letters = string.ascii_letters + string.digits
-    random_string = ''.join(random.choices(letters, k=length))
+    random_string = ''.join(random.choice(letters) for i in range(length))
     return random_string
 
 
