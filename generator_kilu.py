b172288803f243fcf29cdec1373bb6be5642a93f
kiluchandran <kiluchandran@gmail.com>
2024-02-27 16:33:28 +0530

    Generated password with numbers in them

--- generator_kilu.py
+++ generator_kilu.py
@@ -4,7 +4,7 @@ import string
 
 def main():
     N = 7
-    letters = string.ascii_uppercase + string.ascii_lowercase
+    letters = string.ascii_uppercase + string.ascii_lowercase + string.digits
     password = ''.join(random.choices(letters, k=N))
     print(password)
 
