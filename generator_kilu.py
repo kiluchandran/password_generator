0996e81c732232dca868baf3729c7183afe525d4
kiluchandran <kiluchandran@gmail.com>
2024-02-27 17:12:58 +0530

    Generated password with punctuations in them

--- generator_kilu.py
+++ generator_kilu.py
@@ -3,7 +3,7 @@ import string
 
 
 def generate_random_string(length):
-    letters = string.ascii_letters + string.digits
+    letters = string.ascii_letters + string.digits + string.punctuation
     random_string = ''.join(random.choice(letters) for i in range(length))
     return random_string
 
